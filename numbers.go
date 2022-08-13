036ae30e49413c31e5a7fc65db32f8833843ae1c
maxerokhovets <maxerokhovets@gmail.com>
2022-08-13 20:22:27 +0300

    numbers add minmax function

--- numbers.go
+++ numbers.go
@@ -16,6 +16,7 @@ func main() {
 		numbers = append(numbers, num)
 	}
 	evenoddnumb(numbers)
+	maxminnumber(numbers)
 }
 
 func evenoddnumb(numbers []int) {
@@ -31,5 +32,23 @@ func evenoddnumb(numbers []int) {
 	fmt.Println(even)
 	fmt.Print("Нечетные числа: ")
 	fmt.Println(odd)
+
+}
+
+func maxminnumber(numbers []int) {
+	max := numbers[0]
+	min := numbers[0]
+	for _, num := range numbers[1:] {
+		if num > max {
+			max = num
+		}
+		if num < min {
+			min = num
+		}
+	}
+	fmt.Print("Максимальное число: ")
+	fmt.Println(max)
+	fmt.Print("Минимальное число: ")
+	fmt.Println(min)
 
 }
